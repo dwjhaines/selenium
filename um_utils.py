e13b5ab21d4b746ccbff790129eb3394a1e4ef63
Dave Haines <David.Haines@s-a-m.com>
2016-08-23 17:03:26 +0100

    Added support for connection to SQL database

--- um_utils.py
+++ um_utils.py
@@ -1,4 +1,5 @@
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
@@ -6,6 +7,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 from selenium.webdriver.common.action_chains import ActionChains
+
+os.environ["LICENSE_5_USERS"] = 'PVG-standalone-in-house-only-5-users----------------814755e6-01f2-c7e7-5971-cac39bde5fbb525b7c9c-0dab-45b3-ad29-8d282ef2cb81fae144dd-d44874a6b-a28a-7d330028771fae144e06-63ff-4d69-b087-337badbb7c52ca7ca81f-5069-4b61-8799-d58c5e9e9d459f42d31f-cccf-426b-b839-e3c3a9c32fba5cf15e77-b64f-4de9-b88e-114fba3fb67a755c4441-b3de-4a48-a20f-3bd142cd2d8f566fb6a8-3b20-451b-b74f-06da965efbe5e85a72a9-e941-465d-a73b-275205fb55ae3a265fb4-ea8e-45ba-8a9e-d12a3143497779389c8b-8cf9-49f7-84a5-a56b7529771fa003f339-614d-431b-a15c-6e76d6654efa80c80a5e-1772-411d-bbe5-ad8f0fa95439a4c86fdf-bb50-40c8-9f79-3c98c34461ea6b42520a-91a9-4fd3-9558-d6fc6d6bd42cd9ea5b42-de91-4cdd-81ec-820bd824c03f29b6c5ae-c6b8-462c-9e16-b53c1ecddf64543e2763-9548-4f7f-be53-2e104a0b337667b94404-55ad-4d0d-89c7-b2cbae172d3f62fc523d-656d-41e9-bdeb-eabaed3d3da3c53d8cb5-fb2f-408f-970c-a6267740b9ce02a4aec5-199a-48e5-b7a3-cb3969480e6a916f07c5-0733-4790-82ca-beeb67bbdb598f4e3261-3ffa-4237-b95d-d11e6a897fa58b0402e1-e94a-4013-904a-42983a3fd5dbcc769362-5c42-4fae-a7dd-55b5133e8002'
 
 class user:
     def __init__(self, username, password):
--- umtest.py
+++ umtest.py
@@ -1,15 +1,31 @@
 import time
 import um_utils
+import db_utils
 from selenium import webdriver
+import pyodbc
 
 if __name__ == "__main__":
+    
+    # Set up connection to database
+    connection = db_utils.connectToDb()    
+    cur = connection.cursor()
+    
+    # Delete all existing licenses
+    db_utils.deleteAllLicenses(connection, cur)
+    
+    # Install license for five users
+    db_utils.addFiveUserLicense(connection, cur)
+    
+    # Close connection to database
+    db_utils.closeConnection(connection, cur) 
+    
     # List of editors i.e. users that do not have admin rights
-    editors = ['chloe.anderson', 'chloe.garcia', 'chloe.jackson']#, 'chloe.johnson', 'chloe.jones', 'chloe.lee', 'chloe.lewis', 'chloe.martin', 'chloe.martinez', 'chloe.miller', 'chloe.moore', 'chloe.robinson'] 
+    editors = ['chloe.anderson', 'chloe.garcia']#, 'chloe.jackson', 'chloe.johnson', 'chloe.jones', 'chloe.lee', 'chloe.lewis', 'chloe.martin', 'chloe.martinez', 'chloe.miller', 'chloe.moore', 'chloe.robinson'] 
     # List of administrators i.e. users with administrator rights
     admins = ['avaa.johnsona', 'avaa.whitea', 'avac.whitec', 'avad.johnsond', 'avaf.whitef', 'avag.johnsong', 'avag.wilsong', 'avai.robinsoni', 'aval.wilsonl', 'avag.whiteg', 'avah.wilsonh', 'avam.robinsonm']
     # Empty list to be filled with user objects
     users = [] 
-    
+    '''
     for editor in admins:
         # For each editor, create a user object and add object to users list
         users.append(um_utils.user(editor, 'quantel@'))
@@ -29,4 +45,4 @@ if __name__ == "__main__":
             user.loggedin = False
         time.sleep( 1 )
         um_utils.closeBrowser(user)
-
+    '''
